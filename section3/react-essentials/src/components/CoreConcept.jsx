3d156fee4127483420ed14ed3bf9661ae19eae5d
Daniel C <danielcoco96@gmail.com>
2023-11-22 14:11:51 +0100

    section 1-3

---
+++ section3/react-essentials/src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+import {CORE_CONCEPTS} from "../data.js";
+
+export default function CoreConcept({title, description, image}){
+    return <li>
+        <img src={image} alt={title}/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </li>
+}
