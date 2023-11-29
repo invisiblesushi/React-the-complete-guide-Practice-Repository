0c298a4a3b84cc38c17121966281faa48f6739f0
Daniel C <danielcoco96@gmail.com>
2023-11-29 12:06:33 +0100

    Investment calculator

---
+++ section5/investment-calculator/src/components/header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function Header(){
+    return(
+        <div id="header">
+            <header>
+                <img src={logo} alt="logo" />
+                <h1>Investment Calculator</h1>
+            </header>
+        </div>
+    )
+}
