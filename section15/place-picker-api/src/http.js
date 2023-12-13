a8e18015715e51ed879d8bfe346e54c4259a2c76
Daniel C <danielcoco96@gmail.com>
2023-12-13 12:22:55 +0100

    section 15 bugfix

--- section15/place-picker-api/src/http.js
+++ section15/place-picker-api/src/http.js
@@ -25,16 +25,16 @@ export async function fetchUserPlaces(){
 export async function updateUserPlaces(places){
     const response = await fetch('http://localhost:3000/user-places', {
         method: 'PUT',
-        body: JSON.stringify({places: places}),
+        body: JSON.stringify({places}),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         }
     })
 
     const resData = await response.json();
-    
-    if (!resData.ok){
-        throw new Error('Failed to update user data.')
+
+    if (!response.ok) {
+        throw new Error('Failed to update user data.');
     }
     
     return resData.message;
