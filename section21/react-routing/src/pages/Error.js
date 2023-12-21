659bc8b1044121fb5414b1ecfc34165120ff3e5b
Daniel C <danielcoco96@gmail.com>
2023-12-21 15:16:03 +0100

    Section 21

---
+++ section21/react-routing/src/pages/Error.js
@@ -0,0 +1,14 @@
+﻿import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage(){
+    return(
+            <>
+                <MainNavigation/>
+                <main>
+                    <h1>An error occured!</h1>
+                </main>
+            </>
+        )
+}
+
+export default ErrorPage;
---
+++ section21/react-routing/src/pages/Root.js
@@ -0,0 +1,15 @@
+﻿import {Outlet} from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import classes from './Root.module.css'
+
+function RootLayout() {
+    return <>
+        <MainNavigation></MainNavigation>
+        <main className={classes.content}>
+            <Outlet></Outlet>
+        </main>
+    </>
+    
+}
+
+export default RootLayout;
