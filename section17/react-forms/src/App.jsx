0bf9fb6b1c6e15e0ae2a8c20e04497577ef8dd6e
Daniel C <danielcoco96@gmail.com>
2023-12-21 15:15:56 +0100

    Section 17

---
+++ section17/react-forms/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header.jsx';
+import Signup from "./components/SignUp.jsx";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Signup />
+      </main>
+    </>
+  );
+}
+
+export default App;
